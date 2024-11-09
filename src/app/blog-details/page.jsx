af5d60291bd73f75896b05989f9c2c46a406f17e
Sohel Sheikh <sohelxsheikh@gmail.com>
2024-11-09 23:11:44 +0530

    added many feature

--- src/app/blog-details/page.jsx
+++ src/app/blog-details/page.jsx
@@ -1,7 +1,9 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
+
 import {
   Card,
   CardContent,
@@ -9,74 +11,85 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Button } from '@/components/ui/button'
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
-    
-    const router = useRouter()
+  const router = useRouter();
 
-    const searchParam = useSearchParams()
-    const blogId = searchParam.get('id')
-    const [blogData, setBlogData] = useState({
-      title:"",
-      description:""
-    })
-    useEffect( ()=>{
-     setBlogDetail()
-     
-    },[])
+  const searchParam = useSearchParams();
+  const blogId = searchParam.get("id");
+  const [skeleton, setSkeleton] = useState(true);
+  const [blogData, setBlogData] = useState({
+    title: "",
+    description: "",
+  });
+  useEffect(() => {
+    setBlogDetail();
+  }, []);
 
-    const setBlogDetail = async()=>{
-      const mainData = await setBlogDetailData()
-      const { title, description } = mainData
+  const setBlogDetail = async () => {
+    const mainData = await setBlogDetailData();
+    const { title, description } = mainData;
 
-      setBlogData({
-        title,
-        description,
-      })
-      
+    setBlogData({
+      title,
+      description,
+    });
+    setSkeleton(false);
+  };
+
+  const setBlogDetailData = async () => {
+    try {
+      const apiResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_MAIN_URL}/api/get-blog-detail?id=${blogId}`,
+        {
+          method: "GET",
+          cache: "no-store",
+        }
+      );
+
+      console.log("Allah");
+
+      const result = await apiResponse.json();
+
+      return result?.data;
+    } catch (err) {
+      console.log(err);
     }
-
-    const setBlogDetailData = async()=>{
-      try{
-
-        const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_MAIN_URL}/api/get-blog-detail?id=${blogId}`,{
-          method:"GET",
-          cache:"no-store"
-        })
-
-        console.log("Allah")
-       
-        const result = await apiResponse.json();   
-         
-        return result?.data;
-    
-      }catch(err){
-        console.log(err);
-        
-      }
-         
-    }
+  };
 
   return (
     <div>
-      <Card className="mx-5 my-3">
-      <CardHeader>
-        <CardTitle className="text-4xl">{blogData.title}</CardTitle>
-        <CardDescription>You have 3 unread messages.</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4 text-gray-700">
-         <p>{blogData.description}</p>
-      </CardContent>
-      <CardFooter>
-        <Button onClick={()=> router.push('/blog-display')} className="w-full bg-blue-600 text-white md:px-5">
-          Go Back
-        </Button>
-      </CardFooter>
-    </Card>
+      {skeleton ? (
+        <div className="flex flex-col space-y-3 p-7">
+          <Skeleton className="h-60 w-full rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-14 w-[90vw]" />
+            <Skeleton className="h-14 w-[85vw]" />
+          </div>
+        </div>
+      ) : (
+        <Card className="mx-5 my-3">
+          <CardHeader>
+            <CardTitle className="text-4xl">{blogData.title}</CardTitle>
+            <CardDescription>Have A Nice Day...</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4 text-gray-700">
+            <p>{blogData.description}</p>
+          </CardContent>
+          <CardFooter>
+            <Button
+              onClick={() => router.push("/blog-display")}
+              className="w-full bg-blue-600 text-white md:px-5"
+            >
+              Go Back
+            </Button>
+          </CardFooter>
+        </Card>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/index"
 import { NavBarServer } from "@/components/Navbar-server/navbar-server";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
