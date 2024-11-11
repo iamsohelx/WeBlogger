33065cda37dc6c6d36dbad620260f30fd8efe378
Sohel Sheikh <sohelxsheikh@gmail.com>
2024-11-11 10:49:31 +0530

    done

--- src/app/blog-display/page.jsx
+++ src/app/blog-display/page.jsx
@@ -52,15 +52,15 @@ const Page = () => {
             <div className="flex flex-col space-y-3 p-10">
               <Skeleton className="h-[125px] w-full rounded-xl" />
               <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
                 <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[170px]" />
               </div>
             </div>
             <div className="flex flex-col space-y-3 p-10">
               <Skeleton className="h-[125px] w-full rounded-xl" />
               <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
                 <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[170px]" />
               </div>
             </div>
           </div>
